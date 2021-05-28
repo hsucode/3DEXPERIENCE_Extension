a8f7c8582c5b184f6825d48ef258aa0fe454989c
xuscode <song_hsu@126.com>
2021-05-28 23:46:44 +0800

    Add new

--- B27/005-CATArcGuardrail.bat
+++ B27/005-CATArcGuardrail.bat
@@ -19,12 +19,13 @@ set CATPath=C:\Program Files\Dassault Systemes\B27
 set CATEnvName=CATIA_P3.V5-6R2017.B27
 Set CATEnvPath="C:\Program Files\Dassault Systemes\B27\CATEnv"
 set ModuleName=CATArcGuardrail.exe
-set CATFilepath=C:\Users\DESKTOP\Downloads\Part2.CATPart
+set CATFilepath=   C:\Users\DESKTOP\Downloads\udfdemo.CATPart
+set CATFileUDFpath=C:\Users\DESKTOP\Downloads\railUDF.CATPart
 set CrvSetName="CRV"
 set AxisSetName="AXISS"
 
 rem 以下内容请勿修改
-"%CATPath%\win_b64\code\bin\CATSTART.exe" -run "%MyPath%win_b64\code\bin\%ModuleName% %CATFilepath% %CrvSetName% %AxisSetName%" -env %CATEnvName% -direnv %CATEnvPath%
+"%CATPath%\win_b64\code\bin\CATSTART.exe" -run "%MyPath%win_b64\code\bin\%ModuleName% %CATFilepath% %CrvSetName% %AxisSetName% %CATFileUDFpath%" -env %CATEnvName% -direnv %CATEnvPath%
 
 ::cd %~dp0
 ::"C:\Program Files\Dassault Systemes\B27\win_b64\code\bin\CATSTART.exe"  -run "win_b64\code\bin\CATArcWallBatch.exe C:\wall_demo.CATPart" -env CATIA_P3.V5-6R2017.B27 -direnv "C:\Program Files\Dassault Systemes\B27\CATEnv"
